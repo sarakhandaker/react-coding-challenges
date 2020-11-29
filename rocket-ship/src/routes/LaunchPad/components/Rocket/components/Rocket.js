8952e2d5908b2455d76e30fd17f66aae0eaa2ec4
sarakhandaker <kttsara@gmail.com>
2020-11-29 14:18:26 -0800

    fixed re-render

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,13 +1,19 @@
-import React, { useState, Component } from 'react';
+import React, { useState, PureComponent } from 'react';
 import RocketCore from './RocketCore';
 
-export function FunctionalRocket() {
+export const FunctionalRocket = React.memo(props => {
   const [initialLaunchTime] = useState(Date.now());
 
   return <RocketCore initialLaunchTime={initialLaunchTime} />;
 }
+)
+/*
+  Changed Component to PureComponent.
+  React's PureComponent does a shallow compare on the component's props and state. 
+  If nothing has changed, it prevents the rerender of the component.
+*/
 
-export class ClassRocket extends Component {
+export class ClassRocket extends PureComponent {
   constructor() {
     super();
 
