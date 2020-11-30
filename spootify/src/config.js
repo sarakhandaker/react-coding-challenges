0535a032802bc6d374db16a7ff6bd855f1664cba
sarakhandaker <kttsara@gmail.com>
2020-11-29 17:00:40 -0800

    added calls to api

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: "70fbea05217641bba3294073e7e536d6",
+    clientSecret: "367230dd4ed14868a26c551869ab713b"
   }
 }
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+import makeRequest from "../api/makeRequest";
 
 export default class Discover extends Component {
   constructor() {
@@ -11,6 +12,17 @@ export default class Discover extends Component {
       playlists: [],
       categories: []
     };
+  }
+  componentDidMount() {
+    makeRequest("new-releases").then((res) => {
+      this.setState({ newReleases: res.data.albums.items });
+    });
+    makeRequest("featured-playlists").then((res) => {
+      this.setState({ playlists: res.data.playlists.items });
+    });
+    makeRequest("categories").then((res) => {
+      this.setState({ categories: res.data.categories.items });
+    });
   }
 
   render() {
